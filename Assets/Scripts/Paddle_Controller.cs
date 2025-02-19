1fe24947205a4f58854d83bbdeb37c87bd56d529
Tuan_Anh <tuananhwtpq@gmail.com>
2025-02-19 23:02:00 +0700

    Add paddle and background

---
+++ Assets/Scripts/Paddle_Controller.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle_Controller : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(input, 0.0f, 0.0f) * speed * Time.deltaTime;
+        transform.Translate(movement);
+        
+        Vector3 posClamp = transform.position;
+        posClamp.x = Mathf.Clamp(posClamp.x, -9.0f, 9.0f);
+        transform.position = posClamp;
+
+    }
+}
